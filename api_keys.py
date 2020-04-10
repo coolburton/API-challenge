3c8a457bbf4331b8bef8edae02f810b9fe4d5524
Siqing Cao <61628500+coolburton@users.noreply.github.com>
2020-04-10 13:12:19 -0500

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "04cf007b11f4ad6ca00b3c629e3e3665"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyDUj4-kkjdejxPKKshqysNNWJ8teOV_L_M"
+g_key = ""
